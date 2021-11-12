eed262faea81ff66dbe955639ac99c7a462a6027
faysaljafry <faysaljafryofficial@gmail.com>
2021-11-12 21:15:22 +0500

    changed Address in api

--- src/services/API.js
+++ src/services/API.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 export default () => {
   return axios.create({
-    baseURL: `http://localhost:8083/`, // the url of our server
+    baseURL: `https://vueweather-pwa.herokuapp.com/`, // the url of our server
   });
 };
